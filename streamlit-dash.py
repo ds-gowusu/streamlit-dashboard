4b8fd08563b88d3d09b8b631bb68013f94e35807
ds-gowusu <ds.gowusu1985@gmail.com>
2026-06-03 08:45:32 +0000

    inital commit

---
+++ streamlit-dash.py
@@ -0,0 +1,89 @@
+import streamlit as st 
+import pandas as pd
+
+from utils import * 
+
+st.set_page_config(layout='wide')
+
+
+# Load data
+#file_path = r"~/Desktop/projects/Python/py_envs/forest_ml/provenance_psp_data.csv"
+file_path = r"C:\Users\GILBERT FG\Desktop\Projects\portfolio\streamlit_proj\data\provenance_psp_data.csv"
+
+df = pd.read_csv(file_path)
+
+# compute plot dimension (21m x 21m) into hectare
+plot_size = (21*21)/10000
+
+# Basal Area 
+ba_df = calculate_basal_area_per_hectare_by_plot(df, plot_size)
+ba_df = aggregate_basal_area_per_hectare_by_provenance(ba_df)
+
+# Stems Per Hectare
+spha_df = calculate_stems_per_hectare_by_plot(df, plot_size)
+spha_df = aggregate_stems_per_hectare_by_provenance(spha_df)
+
+ba_age = calculate_basal_area_per_hectare_by_age_and_provenance(df, plot_size)
+
+
+# ----------------------------------
+# Streamlit Layouts
+# ----------------------------------
+
+# Layout into 2 columns
+col1, col2 = st.columns(2, gap='medium')
+
+with col1:
+    st.markdown("##### Basal Area Per Provenances")
+
+    st.bar_chart(
+        data=ba_df,
+        x='Provenance2', 
+        y='ba_per_ha', 
+        x_label='Provenance',
+        y_label='Basal Area (m2/ha)'
+    )
+
+with col2:
+    st.markdown("##### Stands Per Hectare")
+
+    st.bar_chart(
+        data=spha_df,
+        x='Provenance2',
+        y='stems_per_ha',
+        x_label='Provenance',
+        y_label='Stems Per Hectare'
+    )
+
+
+# Basal Area Based on Age 
+with st.container():
+    st.markdown("#### Basal Area Per Provenances Based on Age")
+
+    ages = sorted(ba_age['Age'].unique())
+    age_option = st.segmented_control(
+        "Select Age",
+        options=["All"] + list(ages),
+        default="All"
+    )
+    
+    filtered_df = None 
+
+    if age_option == "All":
+        filtered_df = ba_age
+    else:
+        filtered_df = filter_basal_for_age(ba_age, age_option)
+
+    #filtered_df
+
+    # --------------------
+    # Plot view
+    # --------------------
+    st.bar_chart(
+        data=filtered_df,
+        x='Provenance2',
+        y='ba_per_ha',
+        x_label="Provenance",
+        y_label="Basal Area (m2/ha)"
+    )
+
---
+++ utils.py
@@ -0,0 +1,39 @@
+# Stems per Hectare
+def calculate_stems_per_hectare_by_plot(df, plot_size):
+    spha = df.groupby(['Provenance2', 'Plot']).size().reset_index(name='stems_per_plot')
+    spha['stems_per_ha'] = spha['stems_per_plot'] /plot_size
+
+    return spha
+
+def aggregate_stems_per_hectare_by_provenance(df):
+    v_spha = df.groupby('Provenance2')['stems_per_ha'].mean().reset_index()
+
+    return v_spha
+
+
+# Basal Area 
+def calculate_basal_area_per_hectare_by_plot(df, plot_size):
+    ba_ha = df.groupby(['Provenance2', 'Plot'])['basal_area_m2'].sum().reset_index()
+
+    ba_ha['ba_per_ha'] = ba_ha['basal_area_m2'] / plot_size 
+
+    return ba_ha
+
+def aggregate_basal_area_per_hectare_by_provenance(df):
+    ba_prov = df.groupby('Provenance2')['ba_per_ha'].mean().reset_index()
+
+    return ba_prov
+
+
+# Basal Area per Provenances by Ages
+def calculate_basal_area_per_hectare_by_age_and_provenance(df, plot_size):
+    ba_age = df.groupby(['Age', 'Provenance2'])['basal_area_m2'].sum().reset_index()
+    ba_age['ba_per_ha'] = ba_age['basal_area_m2'] / plot_size
+    
+    return ba_age
+
+def filter_basal_for_age(df, selected_age):
+    ba = df[df['Age'] == selected_age]
+
+    return ba
+
